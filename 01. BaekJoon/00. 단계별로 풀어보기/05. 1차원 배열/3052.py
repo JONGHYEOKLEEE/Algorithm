09198c7224b0d171db64749c69c68242e9044996
이종혁 <leejh0317@kyungshin.co.kr>
2021-11-24 13:09:50 +0900

    refactor : m

--- 01. BaekJoon/00. 단계별로 풀어보기/05. 1차원 배열/3052.py
+++ 01. BaekJoon/00. 단계별로 풀어보기/05. 1차원 배열/3052.py
@@ -1,10 +1,8 @@
-arr = list()
 remain = list()
 cnt = 0
 
 for i in range(10):
-    arr.append(int(input()))
-    remain.append(arr[i]%42)
+    remain.append(int(input())%42)
 
 result = set(remain)
 
