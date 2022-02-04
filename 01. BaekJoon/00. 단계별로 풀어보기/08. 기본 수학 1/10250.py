722cb0dd8fa9c4f53c5bdfc1f0d1f9a7efe2e071
이종혁 <leejh0317@kyungshin.co.kr>
2022-02-04 10:33:46 +0900

    feat : solved 10250

--- 01. BaekJoon/00. 단계별로 풀어보기/08. 기본 수학 1/10250.py
+++ 01. BaekJoon/00. 단계별로 풀어보기/08. 기본 수학 1/10250.py
@@ -2,5 +2,12 @@ n = int(input())
 
 for i in range(n):
     H, W, N = map(int, input().split())
-
-print(H, W, N)
+    
+    if N%H == 0:
+        num = N//H
+        floor = H
+    else:
+        num = N//H + 1
+        floor = N%H
+        
+    print(f'{floor*100+num}')
