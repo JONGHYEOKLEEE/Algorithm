31c2f13ddf2bb1c63cb4a193ebcd9cba37201b02
이종혁 <leejh0317@kyungshin.co.kr>
2022-05-09 13:48:37 +0900

    solved

---
+++ 01. BaekJoon/01. 알고리즘 분류/00. 그리디 알고리즘/11399.py
@@ -0,0 +1,14 @@
+from operator import indexOf
+
+
+n = int(input())
+time = list(map(int, input().split()))
+result = 0
+
+time.sort()
+
+for i in range(n):
+    for j in range(i+1):
+        result += time[j]
+
+print(result)
